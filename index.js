47198d27c456740b61d489082b28eaa67172dd51
ERROR <jimenezhugo1232@hotmail.fr>
2025-06-27 17:27:04 +0200

    .

--- index.js
+++ index.js
@@ -30,15 +30,15 @@ async function scriptVote() {
 
     let mainResult = await mainModule(browser, page);
     let successText, captcha;
-    if (mainResult == null) {
-        console.error("mainModule a retourné null, arrêt du script.");
-        await browser.close();
-        return;
-    }
     if (typeof mainResult === 'number') {
         console.log(`mainModule a retourné un nombre (${mainResult}), attente de ${mainResult} minutes...`);
         await sleep((mainResult * 60 * 1000)); // Convertir les minutes en millisecondes
         mainResult = await mainModule(browser, page);
+    }
+    if (mainResult == null) {
+        console.error("mainModule a retourné null, arrêt du script.");
+        await browser.close();
+        return;
     }
     if (Array.isArray(mainResult)) {
         [successText, captcha] = mainResult;
